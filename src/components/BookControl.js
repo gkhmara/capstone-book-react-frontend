7cd7f52dfaf2c8fc658ba392b152a2bed8251acb
Greg Khmara <gkhmara@gmail.com>
2022-09-29 19:29:43 -0700

    add full CRUD functionality to React App

--- src/components/BookControl.js
+++ src/components/BookControl.js
@@ -7,24 +7,25 @@ class BookControl extends React.Component {
 
     this.state = {
       books: [],
-      title: '', //NEW
-      author: '', //NEW
-      rating: '' //NEW
+      title: '',
+      author: '',
+      rating: '',
+      id: ''
     }
   }
 
   componentDidMount = () => {
 
-    fetch('http://localhost:3001/api/v1/books')
+    fetch(`http://localhost:3001/api/v1/books`)
       .then(response => response.json())
       .then(books => this.setState({ books }));
   }
 
 
-  //NEW --START
+  //CREATE -- START
   create = (e) => {
     // e.prevent.Default();
-    fetch("http://localhost:3001/api/v1/books", {
+    fetch(`http://localhost:3001/api/v1/books`, {
       "method": "POST",
       "headers": {
         "content-type": "application/json",
@@ -44,11 +45,54 @@ class BookControl extends React.Component {
       console.log(err);
     });
   }
+  //CREATE -- END
 
+  //UPDATE -- START
+  update(e) {
+    fetch(`http://localhost:3001/api/v1/books/${this.state.id}`, {
+      "method": "PUT",
+      "headers": {
+        "content-type": "application/json",
+        "accept": "application/json"
+      },
+      "body": JSON.stringify({
+        _id: this.state.id,
+        author: this.state.author,
+        title: this.state.title,
+        rating:this.state.rating
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+  //UPDATE --END
+
+  //DELETE --START
+  delete(e) {
+    fetch(`http://localhost:3001/api/v1/books/${this.state.id}`, {
+      "method": "DELETE",
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+  //DELETE --END --NEW
+
+
+  //HANDLE CHANGE --START
   handleChange(changeObject) {
     this.setState(changeObject)
   }
-  //NEW --END
+  //HANDLE CHANGE --END
 
 
   render() {
@@ -58,49 +102,72 @@ class BookControl extends React.Component {
         <h1>All Books</h1>
         {this.state.books.map((book, index) => 
           <li key={index}>
-            <h3>AUTHOR: {book.author} - TITLE: {book.title} - RATING: {book.rating}</h3>
+            <h3>AUTHOR: {book.author} - TITLE: {book.title} - RATING: {book.rating} - ID: {book.id}</h3>
           </li>
         )}
         <hr />
-        {/* NEW -- START */}
-        <h1>Input Book</h1>
-        {/* AUTHOR */}
-        <label htmlFor="author">
-          Author Name:
-          <input
-            name="author"
-            id="author"
-            type="text"
-            className="form-control"
-            value={this.state.author}
-            onChange={(e) => this.handleChange ({author: e.target.value})} required />
-        </label>
-        {/* TITLE */}
-        <label htmlFor="title">
-          Title:
-          <input
-            name="title"
-            id="title"
-            type="text"
-            className="form-control"
-            value={this.state.title}
-            onChange={(e) => this.handleChange ({title: e.target.value})} required />
-        </label>
-        {/* RATING */}
-        <label htmlFor="rating">
-          Rating:
-          <input
-            name="rating"
-            id="rating"
-            type="text"
-            className="form-control"
-            value={this.state.rating}
-            onChange={(e) => this.handleChange ({rating: e.target.value})} required /> 
-        </label>
-        <button className="btn btn-primary" type="button" onClick={(e) => this.create(e)}>
-          Add Book
-        </button> 
-        {/* NEW -- END  */}
+        <form className="d-flex flex-column">
+          <legend className="text-center">Add-Update-Delete Book</legend>
+          <h1>Input Book</h1>
+          {/* AUTHOR */}
+          <label htmlFor="author">
+            Author Name:
+            <input
+              name="author"
+              id="author"
+              type="text"
+              className="form-control"
+              value={this.state.author}
+              onChange={(e) => this.handleChange ({author: e.target.value})} 
+              required
+            />
+          </label>
+          {/* TITLE */}
+          <label htmlFor="title">
+            Title:
+            <input
+              name="title"
+              id="title"
+              type="text"
+              className="form-control"
+              value={this.state.title}
+              onChange={(e) => this.handleChange ({title: e.target.value})} 
+              required 
+            />
+          </label>
+          {/* RATING */}
+          <label htmlFor="rating">
+            Rating:
+            <input
+              name="rating"
+              id="rating"
+              type="text"
+              className="form-control"
+              value={this.state.rating}
+              onChange={(e) => this.handleChange ({rating: e.target.value})} required /> 
+          </label>
+          {/* ID */}
+          <label htmlFor="id">
+            Book ID:
+            <input
+              name="id"
+              id="id"
+              type="text"
+              className="form-control"
+              value={this.state.id}
+              onChange={(e) => this.handleChange({id: e.target.value})}
+            />
+          </label>
+          <button className="btn btn-primary" type="button" onClick={(e) => this.create(e)}>
+            Add Book
+          </button>
+          <button className="btn btn-info" type="button" onClick={(e) => this.update(e)}>
+            Update Book
+          </button>
+          <button className="btn btn-danger" type="button" onClick={(e) => this.delete(e)}>
+            Delete Book
+          </button> 
+        </form>
       </div>
       
     );
