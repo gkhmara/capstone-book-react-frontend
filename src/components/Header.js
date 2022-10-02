08b857bb23064b8e493831c872f4b0f1d6b94372
Greg Khmara <gkhmara@gmail.com>
2022-10-02 15:48:37 -0700

    add basic style to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Box from '@mui/material/Box';
 
 function Header() {
   return (
     <>
-      <h1>Welcome to your Book List!</h1>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+            '& > *': {
+            m: 1,
+        },
+        }}
+      >
+        <h1>Welcome to your Book List!</h1>
+      </Box>
     </>
   );
 }
