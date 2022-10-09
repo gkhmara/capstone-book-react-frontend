0efb2a282a4a43874115105653112418d9455177
Greg Khmara <gkhmara@gmail.com>
2022-10-08 20:41:13 -0700

    styling update

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-// import Stack from '@mui/material/Stack';
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
 
 const BookDetail = () => {
   const [state, setState] = useState({});
 
   const location = useLocation();
   const bookId = location.pathname.split("/")[2];
+  const isRatingValid =
+    (state.rating <= 10 && state.rating >= 0) || state.rating === "";
+  const defaultImage =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Book_red%3B_question_marks.svg/2000px-Book_red%3B_question_marks.svg.png";
 
-  //CUSTOM NAV --START
   const navigate = useNavigate();
   const deleteOnClick = (e) => {
     navigate(-1);
     deleteBook(e);
   };
-  //CUSTOM NAV --END
 
   const getBook = () => {
     fetch(`http://localhost:3001/api/v1/books/${bookId}`)
@@ -24,7 +28,6 @@ const BookDetail = () => {
       .then((book) => setState(book));
   };
 
-  //UPDATE -- START
   const update = (e) => {
     fetch(`http://localhost:3001/api/v1/books/${state.id}`, {
       method: "PUT",
@@ -37,6 +40,7 @@ const BookDetail = () => {
         author: state.author,
         title: state.title,
         rating: state.rating,
+        cover: state.cover,
       }),
     })
       .then((response) => response.json())
@@ -48,9 +52,7 @@ const BookDetail = () => {
         console.log(err);
       });
   };
-  //UPDATE --END
 
-  //DELETE --START
   const deleteBook = (e) => {
     fetch(`http://localhost:3001/api/v1/books/${state.id}`, {
       method: "DELETE",
@@ -64,7 +66,11 @@ const BookDetail = () => {
         console.log(err);
       });
   };
-  //DELETE --END
+
+  const handleChange = (changeObject) => {
+    console.log(changeObject);
+    setState({ ...state, ...changeObject });
+  };
 
   useEffect(() => {
     getBook();
@@ -78,7 +84,7 @@ const BookDetail = () => {
         sx={{
           display: "flex",
           margin: 10,
-          fontSize: 100,
+          fontSize: 25,
           flexDirection: "column",
           alignItems: "center",
           "& > *": {
@@ -86,25 +92,81 @@ const BookDetail = () => {
           },
         }}
       >
-        {/* <div>{state.id}</div> */}
-        <div>{state.title}</div>
-        <div>by {state.author}</div>
-        <div>{state.rating}/10</div>
-
-        <hr />
+        <Card
+          sx={{
+            padding: "10px",
+            margin: "10px",
+            minWidth: "200px",
+          }}
+        >
+          <img
+            style={{ maxWidth: "400px" }}
+            src={state.cover || defaultImage}
+            alt="cover"
+          />
+          <div>{state.title}</div>
+          <div>by {state.author}</div>
+          <div>{state.rating}/10</div>
+        </Card>
         <Button variant="contained" onClick={(e) => update(e)}>
           Update Book
         </Button>
         <Button variant="contained" onClick={deleteOnClick}>
           Delete Book
         </Button>
-        {/* <Button variant="contained" onClick={(e) => deleteBook(e)}>
-          Delete Book
-        </Button> */}
         <Button variant="contained" onClick={() => navigate(-1)}>
           Return to Books
         </Button>
       </Box>
+
+      <div className="center">
+        <form>
+          <h1>Update Book</h1>
+          <Stack spacing={2} direction="row">
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              name="author"
+              type="text"
+              className="form-control"
+              value={state.author}
+              onChange={(e) => handleChange({ author: e.target.value })}
+              required
+            />
+
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              name="title"
+              type="text"
+              className="form-control"
+              value={state.title}
+              onChange={(e) => handleChange({ title: e.target.value })}
+              required
+            />
+
+            <TextField
+              error={!isRatingValid}
+              id="outlined-basic"
+              variant="outlined"
+              name="rating"
+              type="text"
+              className="form-control"
+              value={state.rating}
+              onChange={(e) => handleChange({ rating: e.target.value })}
+              required
+            />
+
+            <TextField
+              name="cover"
+              type="text"
+              className="form-control"
+              value={state.cover}
+              onChange={(e) => handleChange({ cover: e.target.value })}
+            />
+          </Stack>
+        </form>
+      </div>
     </>
   );
 };
